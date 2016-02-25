2654b083797a98b04bc35363427c73bc33851e3b
Boycey63 <c14518013@mydit.ie>
2016-02-25 21:04:24 +0000

    Test moving background (working)

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,25 +1,48 @@
 import processing.core.*;
 
-public class Pokemon extends PApplet
-{
-	public static void main(String args[]) 
-	{
-	   PApplet.main(new String[] { "--present", "Pokemon" });
+public class Pokemon extends PApplet {
+	int x, y, step;
+	PImage map1;
+
+	public static void main(String args[]) {
+		PApplet.main(new String[] { "--present", "Pokemon" });
 	}
-	
-	public void settings()
-	{
-		size(600,600);
+
+	public void settings() {
+		size(1200,800);
 	}
-	
-	public void setup()
-	{
-		background(255,0,0);
+
+	public void setup() {
+		map1 = loadImage("background.jpg");
+		step = 10;
 	}
-	
-	public void draw()
-	{
-		stroke(255);
-		line(0,0,mouseX,mouseY);
+
+	public void draw() {
+		x = constrain(x, 0, map1.width - (width/2));
+		y = constrain(y, 0, map1.height - (height/2));
+
+		image(map1, -x, -y);
+
+		stroke(255, 0, 0);
+		fill(0, 255, 0);
+		ellipse(width / 2, height / 2, 30, 30);
+	}
+
+	public void keyPressed() {
+		if (key == 'd') {
+			x = x + step;
+		}
+
+		if (key == 'a') {
+			x = x - step;
+		}
+
+		if (key == 'w') {
+			y = y - step;
+		}
+
+		if (key == 's') {
+			y = y + step;
+		}
 	}
 }
