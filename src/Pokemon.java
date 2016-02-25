40334c89d2103ffd185ee50322782ee10004e212
Boycey63 <c14518013@mydit.ie>
2016-02-25 22:21:54 +0000

    Changed background + movement update

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -1,7 +1,7 @@
 import processing.core.*;
 
 public class Pokemon extends PApplet {
-	int x, y, step;
+	int x, y, characterX, characterY , step;
 	PImage map1;
 
 	public static void main(String args[]) {
@@ -9,40 +9,46 @@ public class Pokemon extends PApplet {
 	}
 
 	public void settings() {
-		size(1200,800);
+		size(1200,750);
 	}
 
 	public void setup() {
 		map1 = loadImage("background.jpg");
-		step = 10;
+		characterX = width / 2;
+		characterY = height /2;
+		step = 5;
 	}
 
 	public void draw() {
-		x = constrain(x, 0, map1.width - (width/2));
-		y = constrain(y, 0, map1.height - (height/2));
+		x = constrain(x, 0, map1.width - (width));
+		y = constrain(y, 0, map1.height - (height));
 
 		image(map1, -x, -y);
 
 		stroke(255, 0, 0);
 		fill(0, 255, 0);
-		ellipse(width / 2, height / 2, 30, 30);
+		ellipse(characterX, characterY, 30, 30);
 	}
 
 	public void keyPressed() {
 		if (key == 'd') {
 			x = x + step;
+			characterX = characterX + step;
 		}
 
 		if (key == 'a') {
 			x = x - step;
+			characterX = characterX - step;
 		}
 
 		if (key == 'w') {
 			y = y - step;
+			characterY = characterY - step;
 		}
 
 		if (key == 's') {
 			y = y + step;
+			characterY = characterY + step;
 		}
 	}
 }
