fb0f8ac14f8a8ddac12fa4bd1ea7e40b799f0f2c
Boycey63 <c14518013@mydit.ie>
2016-02-29 23:10:43 +0000

    Extra maps add + move through grass enabled

--- src/InGame.java
+++ src/InGame.java
@@ -5,21 +5,36 @@ class InGame {
 	PImage map2;
 	int x, y;
 	int characterX, characterY;
-	int radius , step;
-	int black, white, backColour;
+	int radius , step ,location;
+	int black, white, green, backColour;
 	Pokemon p5;
+	String[] background = new String[20];
 
 	InGame(Pokemon _p5){
 		p5 = _p5;
-		x = y = 0;
+		location = x = y = 6;
 		characterX = p5.width / 2;
 		characterY = p5.height /2;
 		radius = 30;
 		step = 10;
-		black = p5.color(0, 0, 0);
-		white = p5.color(255, 255, 255);
-		map2 = p5.loadImage("Pallet Town1B.png");
-		map1 = p5.loadImage("Pallet Town1F.png");
+		black = 0;
+		white = -1;
+		green = -16712192;
+		background[0] = "Pallet Town2B.png";
+		background[1] = "Pallet Town2F.png";
+		background[2] = "Pallet Town1B.png";
+		background[3] = "Pallet Town1F.png";
+		background[4] = "part1B.png";
+		background[5] = "part1F.png";
+		background[6] = "part2B.png";
+		background[7] = "part2F.png";
+		background[8] = "part3B.png";
+		background[9] = "part3F.png";
+		background[10] = ".png";
+		background[11] = ".png";
+		background[12] = ".png";
+		map2 = p5.loadImage(background[location]);
+		map1 = p5.loadImage(background[location + 1]);
 	}
 	
 	void displayMap(){
@@ -41,18 +56,38 @@ class InGame {
 			characterX = characterX + step;
 			p5.key = 'm';
 		}
-
-		if (p5.key == 'a' && map2.get(characterX - (radius /2), characterY) == white) {
+		
+		if(p5.key == 'd' && map2.get(characterX + (radius /2), characterY) == green){
+			characterX = characterX + step;
+			p5.key = 'm';
+		}
+		
+		if (p5.key == 'a' && map2.get(characterX - (radius /2), characterY) == white){
 			characterX = characterX - step;
 			p5.key = 'm';
 		}
 		
-		if (p5.key == 'w' && map2.get(characterX, characterY - (radius /2)) == white) {
+		if(p5.key == 'a' && map2.get(characterX - (radius /2), characterY) == green){
+			characterX = characterX - step;
+			p5.key = 'm';
+		}
+		
+		if (p5.key == 'w' && map2.get(characterX, characterY - (radius /2)) == white){
+			characterY = characterY - step;
+			p5.key = 'm';
+		}
+		
+		if(p5.key == 'w' && map2.get(characterX, characterY - (radius /2)) == green){
 			characterY = characterY - step;
 			p5.key = 'm';
 		}
 
 		if (p5.key == 's' && map2.get(characterX, characterY + (radius /2)) == white) {
+			characterY = characterY + step;
+			p5.key = 'm';
+		}
+		
+		if(p5.key == 's' && map2.get(characterX, characterY + (radius /2)) == green){
 			characterY = characterY + step;
 			p5.key = 'm';
 		}
